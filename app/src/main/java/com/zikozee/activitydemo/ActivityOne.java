6a13a9ec34a9cae70fa0554f2d6aa0fbf53a4f49
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-08-16 09:50:51 +0100

    Android Layouts. closes #3

--- app/src/main/java/com/zikozee/activitydemo/ActivityOne.java
+++ app/src/main/java/com/zikozee/activitydemo/ActivityOne.java
@@ -3,6 +3,8 @@ package com.zikozee.activitydemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class ActivityOne extends AppCompatActivity {
 //LIFECYCLE
