afc8d8eddd9c26d1c21f4241bd090bd2ab3ae1a4
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-08-16 10:53:14 +0100

    Setting a click event on ListView. closes #7

--- app/src/main/java/com/zikozee/activitydemo/ActivityOne.java
+++ app/src/main/java/com/zikozee/activitydemo/ActivityOne.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -11,7 +13,7 @@ import android.widget.Toast;
 
 import java.util.Date;
 
-public class ActivityOne extends AppCompatActivity {
+public class ActivityOne extends AppCompatActivity implements AdapterView.OnItemClickListener {
 //LIFECYCLE
     String TAG = "ActivityOne";
 //    TextView mTextView;
@@ -36,6 +38,7 @@ public class ActivityOne extends AppCompatActivity {
         mAdapter.add("eBay");
 
         mListView.setAdapter(mAdapter);
+        mListView.setOnItemClickListener(this);//delegating the clicking event to the current class
     }
 
     @Override
@@ -66,6 +69,12 @@ public class ActivityOne extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.i(TAG, "---onDestroy--");
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        final String item = mAdapter.getItem(i);
+        Toast.makeText(this, "You selected: " + item, Toast.LENGTH_LONG).show();
     }
 
     /*public void clickHandler(View view){
